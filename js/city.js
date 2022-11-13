e7332d9d79ff658e8d9586477bc7a4df261e856e
Safezone <76545938+Safezon@users.noreply.github.com>
2022-11-13 16:17:24 +0700

    Initial commit

---
+++ js/city.js
@@ -0,0 +1,21 @@
+/**
+* Philippine Provinces & Cities/Municipalities on HTML Dropdown
+*
+* @ version 1.0.0
+* @ author Marvic R. Macalintal
+*/
+ var cities = {
+	
+	'Thailand' : [
+		'KhonKaen','Korat',
+		],			
+ }
+
+ var City = function() {
+
+	this.p = [],this.c = [],this.a = [],this.e = {};
+	window.onerror = function() { return true; }
+	
+	
+		return this;
+	}
